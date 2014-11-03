fbccc826027072d5d44825828d465aade182dc73
unknown <yunming.zhu@QT-zhuyunming.qunarservers.com>
2014-11-03 16:23:25 +0800

    all

---
+++ src/main/java/com/renren/ntc/sg/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.renren.ntc.sg.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yunming.zhu
+ * Date: 14-10-23
+ * Time: 下午2:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constants {
+}
