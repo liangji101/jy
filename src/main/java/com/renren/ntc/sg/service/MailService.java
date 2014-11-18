de727526fdff01cf971dce4102f2d3f8a1aac141
unknown <yunming.zhu@QT-zhuyunming.qunarservers.com>
2014-11-18 20:04:51 +0800

    ab

---
+++ src/main/java/com/renren/ntc/sg/service/MailService.java
@@ -0,0 +1,11 @@
+package com.renren.ntc.sg.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yunming.zhu
+ * Date: 14-11-12
+ * Time: 下午3:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class MailService {
+}
