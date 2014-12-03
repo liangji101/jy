ccc6c38c816160e6ed4ebba317334626887beeef
allen2 <tianyixingkai@126.com>
2014-12-03 15:06:24 +0800

    New changelist

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn clean package
 /home/q/tools/bin/stop_tomcat.sh  /home/q/www/cat.com
-mv  /home/q/www/cat.com/webapps/ROOT/   /home/q/www/cat.com/webapps/ROOT`date +%Y%m%d`
+mv  /home/q/www/cat.com/webapps/ROOT/   /home/q/ROOT`date +%Y%m%d`
 mv target/jy-1    /home/q/www/cat.com/webapps/ROOT
-/home/q/tools/bin/start_tomcat.sh  /home/q/www/cat.com/
+/home/q/tools/bin/start_tomcat.sh  /home/q/www/cat.com
