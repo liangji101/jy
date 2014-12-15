8cb9fd3cb8470bf61af26778a8e4b22845c4f448
jiliang <liangji101@yahoo.cn>
2014-12-15 21:50:00 +0800

    update shop cart js

--- src/main/webapp/sg/js/shopAction.js
+++ src/main/webapp/sg/js/shopAction.js
@@ -1,6 +1,57 @@
 
 $.mobile.page.prototype.options.domCache = true;
 
-$( document ).on( "vclick", "a#checkout", function() {
-//    alert("test");
+$(document).ready(function(){
+
+//    $( document ).on( "vclick", "#submit_shop", function() {
+//        var items = [];
+//        for(var idx = 0 ;idx < simpleCart.items().length;idx++){
+//            items.push({'item_id': simpleCart.items()[idx].fields().original ,"count":simpleCart.items()[idx].fields().quantity});
+//        }
+////        values += "&items=" + encodeURIComponent(JSON.stringify(items));
+//
+//        event.preventDefault();
+//
+//        $.ajax({
+//            type: "POST",
+//            url: "shopCar/confirm",
+//            data: JSON.stringify({"shop_id":"1", "items": items}),
+//            dataType: "json",
+//            success: function(data){
+//                with(window.document)
+//                {
+//                    open();
+//                    write(data.responseText);
+//                    close();
+//                }
+//            },
+//            error:function(data){
+//                with(window.document)
+//            {
+//                open();
+//                write(data.responseText);
+//                close();
+//            }
+//            }
+//        })
+//    });
+
+    $( document ).on( "vclick", "#submit_shop", function() {
+
+        var items = [];
+        for(var idx = 0 ;idx < simpleCart.items().length;idx++){
+            items.push({'item_id': simpleCart.items()[idx].fields().original ,"count":simpleCart.items()[idx].fields().quantity});
+        }
+        $('#form_items').val(JSON.stringify(items));
+    });
+
+
+    $("#shopConfirm").submit(function( event ) {
+
+        var values = $(this).serialize();
+
+        return true;
+    });
 });
+
+
