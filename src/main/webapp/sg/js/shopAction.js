f6cbc106181293d8ce2244c4c1557219bed1550b
jiliang <liangji101@yahoo.cn>
2014-12-20 15:52:58 +0800

    update shop view

--- src/main/webapp/sg/js/shopAction.js
+++ src/main/webapp/sg/js/shopAction.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
 
             if($(item).data('categray-id') == cateId){
                 $(item).removeClass('hidden');
+                $('.loadingMore').show();
             }else{
                 $(item).addClass('hidden');
             }
@@ -50,6 +51,61 @@ $(document).ready(function(){
         $('#form_items').val(JSON.stringify(items));
 
     });
+
+    $( document ).on( "vclick", ".loadingMore", function() {
+
+        var me = this;
+        var visiableCate = $('.shop-product-categray-list:not(.hidden)');
+        if(visiableCate){
+            var shop_id = $('#shop_id').val();
+            var cateId = visiableCate.data('categray-id');
+            var from = $('.js-product-item',visiableCate).last().attr('id');
+            var offset = 20;
+
+            $(me).html('<i class="fa fa-spinner fa-spin">点击加载更多</i>');
+            $.getJSON(
+                "shop/getitems?shop_id=" + parseInt(shop_id) +
+                    "&category_id="+ parseInt(cateId)+ "&from="+ parseInt(from) +
+                    "&offset="+offset,
+                function(data) {
+                    if(data.code == 0 && Array.isArray(data.data) && data.data.length > 0){
+                        $(data.data).each(function(idx,item){
+                            $(visiableCate).append(
+                                '<li class="product-item js-product-item" id="'+ item.id +'">' +
+                                 '<input type="hidden" class="js-product-item-id" value="'+ item.id +'"data-value="'+ item.id+ '"/>' +
+                                '<div class="prod_icon" style="background-image: url("'+ item.pic_url + '")">'+ '</div>' +
+                                '<div>'+
+                                '<span class="product-title oneline js-product-item-name" data-value="'+ item.name+'">'+ item.name+ '</span>'+
+                                '#set($itempricemain = $item.price/100)'+
+                                '#set($itempriceleft = $item.price%100)'+
+                                '<div class="prod_info">'+
+                                '<div class="product-price js-product-item-price" data-value="'+ item.price +'">'+
+                                '$itempricemain<span>.$itempriceleft</span>'+
+                                '</div>'+
+                                '<div class="product_stepper">'+
+                                '<div class="product_stepper_minus countChangeAction countChangeActionMinus" style="">-</div>'+
+                                '<div class="product_stepper_count">'+
+                                '<input type="text" class="product_stepper_count_input item-quantity countChangeAction countChangeActionSet hidden js-product-item-quantity" style="padding-top: 2px" value="0" data-value="0"></div>'+
+                                '<div class="product_stepper_add countChangeAction countChangeActionAdd">+</div>'+
+                                '</div>'+
+                                '</div>'+
+                            '</div>'+
+                            '</li>'
+                            )
+                        });
+
+                        $(me).html('<i class="fa">点击加载更多</i>');
+
+                    }else{
+                        // code not 0
+                        $(me).html('<i class="fa">没有更多商品了</i>');
+                        $(me).fadeOut();
+                    }
+                }
+            );
+        }
+    });
+
 
 
     $("#shopConfirm").submit(function( event ) {
