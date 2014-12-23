7dd15634857b450bc174a776d91dab9fa24c3327
jiliang <liangji101@yahoo.cn>
2014-12-23 19:29:10 +0800

    udpate js

--- src/main/webapp/sg/js/userProfile.js
+++ src/main/webapp/sg/js/userProfile.js
@@ -1,5 +1,44 @@
 $(document).ready(function () {
 
+    function updateDefaultAddress(){
+        var selected = $('.addressListSelection .addressSelected').first();
+
+        if(!selected)alert('没有选择任何地址');
+
+        var address = $('.addressItem-address', selected).data('default-address'),
+            phone = $('.addressItem-phone', selected).data('default-phone'),
+            id = $('.addressItem-id', selected).val();
+
+        $('#addressSpinner').removeClass('hidden');
+        $.getJSON('address/default',{ 'address':  address,
+                'phone':phone,
+                'address_id': id},
+            function(status){
+                $('#addressSpinner').addClass('hidden');
+
+                if(status.code == 0){
+
+                    $('#form_shop_id').val(getParameterByName('shop_id'));
+                    $('#form_items').val(shoppingCart.getItemString());
+
+                    var oldaddr = $('#shopConfirm').attr('action');
+                    $('#shopConfirm').attr('action',oldaddr.replace(/shop_id=*/,'shop_id='+getParameterByName('shop_id')));
+
+                    $('.useThisAddress').attr('disabled','disabled');
+
+                    var data = $('#shopConfirm').serialize();
+                    $('#shopConfirm').submit();
+
+                }else{
+                    alert('更改默认地址失败，请再次尝试');
+                }
+            });
+    }
+
+    $(document).on("vclick", ".useThisAddress", function () {
+        updateDefaultAddress();
+    });
+
     $('.order-item').each(function (idx, item) {
 
         var string = $('.order-snapshots-string', item).val();
@@ -37,6 +76,8 @@ $(document).ready(function () {
         }
 
     });
+
+
 });
 
 
